87a8e662c4325d8ed330ce8529f7b89d751abe1f
Laila A. Andersen <lail5198@edu.easj.dk>
2020-10-07 14:19:56 +0200

    I have put the exercise text in now

--- src/Task6_2_SumTheDigitsInAnInteger.java
+++ src/Task6_2_SumTheDigitsInAnInteger.java
@@ -2,6 +2,11 @@ import java.util.Scanner;
 
 public class Task6_2_SumTheDigitsInAnInteger {
 
+    /*  Write a method that computes the sum of the digits
+        in an integer. Use the following method header:
+        public static int sumDigits(long n)
+    */
+
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
